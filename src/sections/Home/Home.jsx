42b76808faefeb8e0273101cc189e5a31d9944e8
Edmir Galvan <edmirgalvaz@gmail.com>
2025-01-27 06:08:22 -0600

    Home ready

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,3 +1,22 @@
+import "./Home.css";
+
 export const Home = () => {
-  return <section className="contentPage">Home</section>;
+  return (
+    <section className="contentPage HomePage">
+      <h2>¡Nos casamos!</h2>
+      <h1>Edmir y Janice</h1>
+      <div className="versicle">
+        <p>
+          <em>
+            Además, uno que anda solo puede ser vencido, pero dos juntos pueden
+            hacerle frente al agresor. Y una cuerda triple no se rompe
+            fácilmente
+          </em>
+        </p>
+        <p>
+          <strong>Eclesiastes 4:12</strong>
+        </p>
+      </div>
+    </section>
+  );
 };
