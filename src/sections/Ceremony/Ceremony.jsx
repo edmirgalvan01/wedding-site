d69041524254a8eabc6b3e30168804cd4003ac98
Edmir Galvan <edmirgalvaz@gmail.com>
2025-01-29 20:02:16 -0600

    Ceremony structure

--- src/sections/Ceremony/Ceremony.jsx
+++ src/sections/Ceremony/Ceremony.jsx
@@ -1,3 +1,25 @@
+import { CiBookmark } from "react-icons/ci";
+import "./Ceremony.css";
+
 export const Ceremony = () => {
-  return <div className="contentPage">Ceremony</div>;
+  return (
+    <section className="contentPage Ceremony">
+      <div className="ceremony--title">
+        <CiBookmark />
+        <p>CEREMONIA</p>
+      </div>
+      <div className="ceremony--content">
+        <p>
+          Dios tiene un gran peso en nuestras vidas. Por eso mismo, te esperamos
+        </p>
+        <div className="ceremony--content__date">
+          <p>15 MAR</p>
+          <div className="divider"></div>
+          <p>14:00</p>
+        </div>
+        <span>2972 Westheimer Rd. Santa Ana, Illinois 85486 </span>
+        <button>Ver ubicacion</button>
+      </div>
+    </section>
+  );
 };
