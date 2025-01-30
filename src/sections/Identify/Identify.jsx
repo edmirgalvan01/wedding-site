879e467a9277536a47acd28ab18466d585fd1f35
Edmir Galvan <edmirgalvaz@gmail.com>
2025-01-29 20:42:43 -0600

    Identify structure

--- src/sections/Identify/Identify.jsx
+++ src/sections/Identify/Identify.jsx
@@ -1,3 +1,31 @@
+import { CiBookmarkCheck } from "react-icons/ci";
+import "./Identify.css";
+
 export const Identify = () => {
-  return <div className="contentPage">Identify</div>;
+  return (
+    <section className="contentPage Identify">
+      <div className="event--title">
+        <CiBookmarkCheck size={50} />
+        <p>PASE DE ENTRADA</p>
+      </div>
+      <div className="identify--content">
+        <p>
+          Son muy importantes en nuestras vidas. Esperamos contar con su
+          presencia
+        </p>
+        <ul>
+          <li>David Lopez</li>
+          <li>Reina Perdomo</li>
+          <li>Karen Lopez</li>
+        </ul>
+        <span>
+          Favor de confirmar dos semanas antes del evento (1 de marzo)
+        </span>
+        <div className="identify--content__buttons">
+          <button>Numero de Janice</button>
+          <button>Numero de Edmir</button>
+        </div>
+      </div>
+    </section>
+  );
 };
