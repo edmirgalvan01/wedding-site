0b22d69a44938e9d451592494689785b170f72c4
Edmir Galvan <edmirgalvaz@gmail.com>
2025-01-29 20:30:00 -0600

    Gallery created

--- src/sections/Gallery/Gallery.jsx
+++ src/sections/Gallery/Gallery.jsx
@@ -1,3 +1,59 @@
+import "./Gallery.css";
+import { CiCamera } from "react-icons/ci";
+
 export const Gallery = () => {
-  return <div className="contentPage">Gallery</div>;
+  return (
+    <section className="contentPage Gallery">
+      <div className="gallery--title">
+        <CiCamera size={50} />
+        <p>GALERIA</p>
+      </div>
+      <div className="gallery--content">
+        <p>
+          Todo lo que hemos logrado no lo hubieramos podido hacer sin ti.
+          Gracias por aportar sabiduria a nuestras vidas
+        </p>
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+        <img
+          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Fotografia de Edmir y Janice"
+        />
+      </div>
+    </section>
+  );
 };
