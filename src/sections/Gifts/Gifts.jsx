c9b82afc33f84ad2a5a7ae2b9714a537771a5b7f
Edmir Galvan <edmirgalvaz@gmail.com>
2025-01-29 20:52:59 -0600

    Gifts created

--- src/sections/Gifts/Gifts.jsx
+++ src/sections/Gifts/Gifts.jsx
@@ -1,3 +1,24 @@
+import { CiGift } from "react-icons/ci";
+import "./Gifts.css";
+
 export const Gifts = () => {
-  return <div className="contentPage">Gifts</div>;
+  return (
+    <section className="Gifts">
+      <div className="event--title">
+        <CiGift size={50} />
+        <p>REGALOS</p>
+      </div>
+      <div className="gifts--content">
+        <p>
+          El mejor regalo es compartir con nosotros este día tan especial, pero
+          si desean ayudarnos, agradeceremos cualquier aporte en efectivo, que
+          será de gran utilidad para comenzar nuestra nueva vida
+        </p>
+        <span>
+          En la entrada del salon de eventos habra una cajita en donde podran
+          poner sus aportaciones
+        </span>
+      </div>
+    </section>
+  );
 };
