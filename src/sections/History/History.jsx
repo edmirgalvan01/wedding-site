e06cec3d8f9d5b68b07049b9f206b9667ecb046e
Edmir Galvan <edmirgalvaz@gmail.com>
2025-02-03 07:16:39 -0600

    History images added

--- src/sections/History/History.jsx
+++ src/sections/History/History.jsx
@@ -12,7 +12,7 @@ export const History = () => {
         <span className="playwrite">Nos hicimos novios</span>
         <h3 className="lato">30 DE JULIO DEL 2023</h3>
         <img
-          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="src/assets/relationship.jpg"
           alt="Foto de Edmir y Janice cuando se hicieron novios"
         />
       </div>
@@ -20,7 +20,7 @@ export const History = () => {
         <span className="playwrite">Nos comprometimos</span>
         <h3 className="lato">7 DE ENERO DEL 2024</h3>
         <img
-          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="src/assets/compromise.jpg"
           alt="Foto de Edmir y Janice cuando se comprometieron"
         />
       </div>
@@ -28,7 +28,7 @@ export const History = () => {
         <span className="playwrite">Nos casaremos</span>
         <h3 className="lato">15 DE MARZO DEL 2025</h3>
         <img
-          src="https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="src/assets/now.JPG"
           alt="Foto de Edmir y Janice en la actualidad"
         />
       </div>
